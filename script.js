3db83fa447a38c0394f704b3f89b94545de2c5f8
kemboi590 <bkemboi590@gmail.com>
2024-05-10 13:24:56 +0300

    feat: add close button functionality to hamburger menu

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 const hamburger = document.querySelector(".hamburger");
+const closeBurger = document.querySelector(".close-burger");
 const menu = document.querySelector(".menu");
 
 hamburger.addEventListener("click", () => {
   menu.classList.toggle("open");
+  closeBurger.style.display = "block"; // Show close button when hamburger is clicked
+});
+
+closeBurger.addEventListener("click", () => {
+  menu.classList.remove("open");
+  closeBurger.style.display = "none"; // Hide close button when close button is clicked
 });
