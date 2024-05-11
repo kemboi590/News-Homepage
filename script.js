488305727c3633fc59cd184e43694d418dc2a7b1
kemboi590 <bkemboi590@gmail.com>
2024-05-12 00:11:15 +0300

    refactor: improve responsiveness of hamburger menu and close button on small screens

--- script.js
+++ script.js
@@ -2,12 +2,19 @@ const hamburger = document.querySelector(".hamburger");
 const closeBurger = document.querySelector(".close-burger");
 const menu = document.querySelector(".menu");
 
+// Hide the close-burger icon when the page loads
+closeBurger.style.display = "none";
+
 hamburger.addEventListener("click", () => {
   menu.classList.toggle("open");
-  closeBurger.style.display = "block"; // Show close button when hamburger is clicked
+  if (menu.classList.contains("open")) {
+    closeBurger.style.display = "block";
+  } else {
+    closeBurger.style.display = "none";
+  }
 });
 
 closeBurger.addEventListener("click", () => {
   menu.classList.remove("open");
-  closeBurger.style.display = "none"; // Hide close button when close button is clicked
+  closeBurger.style.display = "none";
 });
